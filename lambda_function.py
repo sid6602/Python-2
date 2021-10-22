729cbf1ac2790a45d2f32505474717f5c38451fb
Siddhi Mankar <68636373+sid6602@users.noreply.github.com>
2021-10-23 01:33:21 +0530

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+# Lambda function
+
+minus= lambda a,b : a-b
+print("Ans using lambda function: ",  minus(10,5))
+
+# lambda function is alternative of functions like below
+
+def minus(x,y):
+    return x-y
+
+print("Ans using simmple function: ", minus(15,5))
